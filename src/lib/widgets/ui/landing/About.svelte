8f7c96bc3db6a6be9e43fdd3f48ad9168215d88b
DarkDev <darkdev_brand@riseup.net>
2024-07-01 07:02:55 +0300

    refactoring structure

--- src/lib/widgets/ui/landing/About.svelte
+++ src/lib/widgets/ui/landing/About.svelte
@@ -1,28 +1,33 @@
 <section theme="2" class="about-home-section">
       <div  class="container">
-        <div class="col-12">
+        <div class="w-full flex flex-row justify-between flex-wrap">
           <div  class="about-content-one">
-            <h2 class="km-h4">ABOUT US</h2>
+            <h2 class="km-h4">ABOUT</h2>
           </div>
-          <div id="w-node-_239c6446-e890-98e2-63d1-0034b32f6f5b-9eabcae1" class="ab-content-two">
-            <h2 class="km-h2 about split-lines">With 12+ years of experience, KOMMUNA combines Creative + Marketing + Technology to give our partners the most powerful force to drive business growth.</h2>
+          <div  class="ab-content-two">
+            <p class="km-h2 about split-lines">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <p class="km-h2 about split-lines">
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.            </p>
             <div class="padding _6rem"></div>
           </div>
-          <div id="w-node-af8246ac-66ac-fa06-b073-6b4f1cf56c77-9eabcae1" class="div-block-102"></div>
-          <div id="w-node-b5a634bf-45f6-afc2-a23f-e117df760a6a-9eabcae1">
-            <div id="w-node-_00260ed4-f93a-31d4-55a6-1c088537a094-9eabcae1" class="km-h4">KOMMUNA is a Marketing + Technology agency.</div>
+          <div class="div-block-102"></div>
+          <div >
+            <div  class="km-h4">Paleo Stories - educational boardgame</div>
             <div class="padding _6rem"></div>
           </div>
         </div>
       </div>
-      <div id="w-node-_7fb5ddda-fd44-2aec-9baf-e53a86aac3f8-9eabcae1" class="featured-images-mobile">
-        <div id="w-node-c9a272f9-7135-dbbd-6500-1abdb599f792-9eabcae1" class="ft-image-item _1"></div>
-        <div id="w-node-d4837b39-20e6-edc4-95e0-536b568f560f-9eabcae1" class="ft-image-item _2"></div>
-        <div id="w-node-_14af5021-190a-4e6b-96e3-49fc9c9bd1ab-9eabcae1" class="ft-image-item _4"></div>
-        <div id="w-node-_9bec0c32-725f-8903-82b4-40b1e94ac220-9eabcae1" class="ft-image-item _3"></div>
+
+      <div  class="featured-images-mobile">
+        <div class="ft-image-item _1"></div>
+        <div class="ft-image-item _2"></div>
+        <div class="ft-image-item _4"></div>
+        <div  class="ft-image-item _3"></div>
       </div>
       <div class="carrusel-container">
-        <div data-w-id="cfc642e6-cc22-4c8c-12e6-872afc812972" class="scroll-slider-list">
+        <div  class="scroll-slider-list">
           <div class="carrusel-img-div"></div>
           <div class="carrusel-img-div _2"></div>
           <div class="carrusel-img-div _3"></div>
@@ -34,4 +39,3 @@
         </div>
       </div>
     </section>
-    
