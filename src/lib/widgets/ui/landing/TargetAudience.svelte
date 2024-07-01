e8dba62b7109e680febd65425f8efe33417d6cac
DarkDev <darkdev_brand@riseup.net>
2024-07-01 07:03:55 +0300

    refactoring by components

--- src/lib/widgets/ui/landing/TargetAudience.svelte
+++ src/lib/widgets/ui/landing/TargetAudience.svelte
@@ -1,84 +1,50 @@
-<section id="ourwork" theme="2" class="forces-section">
-      <div class="container-fw-r">
-        <div class="col-12">
-          <div id="w-node-a0f125d9-3a5a-9767-f88f-298c9aa5e66d-9eabcae1" class="forces-content-one">
-            <div class="km-h2 split-lines">Three forces combined to bring your brand true superpowers. </div>
+<script>
+  const targetAudience = [
+    [
+      {
+        title: 'Lorem Ipsum is simply dummy text',
+        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
+      },
+      {
+        title: 'Lorem Ipsum is simply dummy text',
+        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
+      },
+    ],
+    [
+      {
+        title: 'Lorem Ipsum is simply dummy text',
+        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
+      },
+      {
+        title: 'Lorem Ipsum is simply dummy text',
+        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
+      },
+    ],
+  ]
+</script>
+
+<section  theme="2" class="why-kommuna">
+      <div class="center-div">
+        <div class="km-h4 whyus">Gamification of paleontology by using technologies.</div>
+        <div class="padding _1rem"></div>
+        <div class="km-h3">Say goodbye to the traditional way of doing things. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+        <div class="padding"></div>
+        {#each targetAudience as audience}
+        <div class="row-why">
+          <div class="why-item">
+            <div class="km-h4">{audience[0].title}</div>
+            <div class="line-white why"></div>
+            <div class="km-p">{audience[0].desc}</div>
           </div>
-          <div id="w-node-a0f125d9-3a5a-9767-f88f-298c9aa5e670-9eabcae1" class="slider-forces_component">
-            <div class="slider-forces_overflow">
-              <div>
-                <div class="slider-forces_wrapper">
-                  <div class="swiper is-slider-forces">
-                    <div class="swiper-wrapper is-slider-forces">
-                      <div class="swiper-slide is-slider-forces">
-                        <div class="km-ornamental">01</div>
-                        <div class="line-white"></div>
-                        <div class="km-h4 title-force">CREATIVE</div>
-                        <div class="padding _1rem"></div>
-                        <div class="km-big-p force-text">Everything has always started with the power of imagination. From a Content Strategy, UX/UI Design, or a compelling TV ad, we combine our experience in Storytelling with technology to build emotional resonance, inspire and build community along your brand.</div>
-                        <div class="padding _3rem"></div>
-                      </div>
-                      <div class="swiper-slide is-slider-forces">
-                        <div class="km-ornamental">02</div>
-                        <div class="line-white"></div>
-                        <div class="km-h4 title-force">MARKETING</div>
-                        <div class="padding _1rem"></div>
-                        <div class="km-big-p force-text">A/B Testing, Demand Generation, Lead Gen, Lead Scoring, Pipelines, ROAS, are some of our everyday keywords, and we are very serious about them. The New Marketing is about bringing you strategies and equations that will bring your brand, not only results, but a consistent omnichannel performance.</div>
-                      </div>
-                      <div class="swiper-slide is-slider-forces">
-                        <div class="km-ornamental">03</div>
-                        <div class="line-white"></div>
-                        <div class="km-h4 title-force">TECHNOLOGY</div>
-                        <div class="padding _1rem"></div>
-                        <div class="km-big-p force-text">Leading Marketing plans and strategies can be a monumental task. Lead Scoring, tasks automation, We push your entire Marketing operation years ahead into the future by providing you with tech tools that bring certainty, efficiency, and collaboration for the best decision making.</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div element-theme="2" class="slider-gallery_button-wrapper">
-              <a href="#" class="slider-gallery_button swiper-prev w-inline-block">
-                <div class="slider-gallery_button-icon w-embed">
-                  <svg width="31" height="25" viewBox="0 0 31 25" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.5 12.5L0.792894 13.2071L0.0857874 12.5L0.792894 11.7929L1.5 12.5ZM29.625 11.5C30.1773 11.5 30.625 11.9477 30.625 12.5C30.625 13.0523 30.1773 13.5 29.625 13.5L29.625 11.5ZM12.0429 24.4571L0.792894 13.2071L2.20711 11.7929L13.4571 23.0429L12.0429 24.4571ZM0.792894 11.7929L12.0429 0.542893L13.4571 1.95711L2.20711 13.2071L0.792894 11.7929ZM1.5 11.5L29.625 11.5L29.625 13.5L1.5 13.5L1.5 11.5Z"></path>
-                  </svg>
-                </div>
-              </a>
-              <a href="#" class="slider-gallery_button swiper-next w-inline-block">
-                <div class="slider-gallery_button-icon is-reversed w-embed">
-                  <svg width="31" height="25" viewBox="0 0 31 25" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.5 12.5L0.792894 13.2071L0.0857874 12.5L0.792894 11.7929L1.5 12.5ZM29.625 11.5C30.1773 11.5 30.625 11.9477 30.625 12.5C30.625 13.0523 30.1773 13.5 29.625 13.5L29.625 11.5ZM12.0429 24.4571L0.792894 13.2071L2.20711 11.7929L13.4571 23.0429L12.0429 24.4571ZM0.792894 11.7929L12.0429 0.542893L13.4571 1.95711L2.20711 13.2071L0.792894 11.7929ZM1.5 11.5L29.625 11.5L29.625 13.5L1.5 13.5L1.5 11.5Z"></path>
-                  </svg>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div id="w-node-af4b4dea-7f3d-4f1c-54cf-db0c67f3f44a-9eabcae1" class="forces-wrapper-mobile">
-            <div id="w-node-_54266363-0866-c674-ebf5-f96d8031bb21-9eabcae1" class="force-item">
-              <div class="km-ornamental split-lines">01</div>
-              <div class="line-white"></div>
-              <div class="km-h4 split-lines">CREATIVE</div>
-              <div class="padding _1rem"></div>
-              <div class="km-big-p split-lines">Everything has always started with the power of imagination. From a Content Strategy, UX/UI Design, or a compelling TV ad, we combine our experience in Storytelling with technology to build emotional resonance, inspire and build community along your brand.</div>
-              <div class="padding _3rem"></div>
-            </div>
-            <div id="w-node-_118d67d6-c4fe-076b-b41b-a50d02a73d5c-9eabcae1" class="force-item">
-              <div class="km-ornamental split-lines">02</div>
-              <div class="line-white"></div>
-              <div class="km-h4 split-lines">MARKETING</div>
-              <div class="padding _1rem"></div>
-              <div class="km-big-p split-lines">A/B Testing, Demand Generation, Lead Gen, Lead Scoring, Pipelines, ROAS, are some of our everyday keywords, and we are very serious about them. The New Marketing is about bringing you strategies and equations that will bring your brand, not only results, but a consistent omnichannel performance.</div>
-            </div>
-            <div id="w-node-_8d70d1ad-c4dc-7fb4-ef67-10ded81e08ac-9eabcae1" class="force-item">
-              <div class="km-ornamental split-lines">03</div>
-              <div class="line-white"></div>
-              <div class="km-h4 split-lines">TECHNOLOGY</div>
-              <div class="padding _1rem"></div>
-              <div class="km-big-p split-lines">Leading Marketing plans and strategies can be a monumental task. Lead Scoring, tasks automation, We push your entire Marketing operation years ahead into the future by providing you with tech tools that bring certainty, efficiency, and collaboration for the best decision making.</div>
-              <div class="swiper-slide is-slider-forces"></div>
+          <div  class="why-item">
+            <div class="km-h4 title-experience">{audience[1].title}</div>
+            <div class="line-white why"></div>
+            <div class="km-p">
+            {audience[1].desc}
             </div>
           </div>
         </div>
+        <div class="padding why"></div>
+        {/each}
       </div>
     </section>
--- src/lib/widgets/ui/landing/TheStories.svelte
+++ src/lib/widgets/ui/landing/TheStories.svelte
@@ -30,23 +30,25 @@
      -->
 
 
-    <section id="mindset" theme="1" class="mindset-section">
+    <section theme="1" class="mindset-section">
       <div class="div-668">
         <div class="container relative">
-          <div class="wrap-mindset">
-            <h1 theme="1" id="w-node-_8b2a2023-7c1f-bed5-bc8e-ed68785ae949-9eabcae1" class="km-h4 mindset">Our mindset defines us.</h1>
-            <div id="w-node-_190fe641-97ca-0c10-7bc6-d91ec1ccd262-9eabcae1" class="mindset-wrap-2">
-              <div id="w-node-_2167d651-0305-e41e-4385-4b90692dde65-9eabcae1" class="km-h3 mindsetright">What happens when you blend an advertising agency and tech company? Our mission is to bring you the future of Marketing, now. Discover how we approach our everyday workflow to accomplish that.</div>
+          <div class="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1">
+            <h1 theme="1" class="km-h4 mindset">Our mindset defines us.</h1>
+            <div  class="mindset-wrap-2">
+              <div class="km-h3 mindsetright">
+                What happens when you blend an advertising agency and tech company? Our mission is to bring you the future of Marketing, now. Discover how we approach our everyday workflow to accomplish that.
+              </div>
               <div class="padding-mindset _3rem"></div>
               <div class="div-block-103">
-                <a data-network="Social Network" button-style="1" theme="1" href="/about" class="button-main-with-link w-inline-block">
+                <a data-network="Social Network" button-style="1" theme="1" href="/" class="button-main-with-link w-inline-block">
                   <svg viewBox="0 0 15 15" fill="none" class="btn_main_icon">
                     <path d="M2.28604 12.214C3.21839 13.1463 4.40627 13.7813 5.69948 14.0385C6.99269 14.2957 8.33313 14.1637 9.5513 13.6591C10.7695 13.1545 11.8107 12.3 12.5432 11.2037C13.2758 10.1074 13.6667 8.81846 13.6667 7.49992C13.6667 6.18138 13.2758 4.89245 12.5432 3.79612C11.8107 2.69979 10.7695 1.84531 9.5513 1.34072C8.33313 0.836138 6.99269 0.704115 5.69948 0.96135C4.40627 1.21858 3.21839 1.85352 2.28604 2.78587" stroke="currentColor" stroke-width="1"></path>
                     <path d="M9 7.50008L9.39043 7.18773L9.64031 7.50008L9.39043 7.81243L9 7.50008ZM1 8.00008C0.723858 8.00008 0.5 7.77622 0.5 7.50008C0.5 7.22394 0.723858 7.00008 1 7.00008V8.00008ZM6.72377 3.8544L9.39043 7.18773L8.60957 7.81243L5.9429 4.4791L6.72377 3.8544ZM9.39043 7.81243L6.72377 11.1458L5.9429 10.5211L8.60957 7.18773L9.39043 7.81243ZM9 8.00008H1V7.00008H9V8.00008Z" stroke="currentColor" stroke-width="0.3" fill="currentColor"></path>
                   </svg>
                   <div class="button-text">ABOUT US</div>
                 </a>
-                <link rel="prefetch" href="/about">
+                <link rel="prefetch" href="/">
               </div>
             </div>
           </div>
