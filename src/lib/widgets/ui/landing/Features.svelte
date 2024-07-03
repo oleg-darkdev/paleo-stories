ed2ef5beea2eeb61ee75f24171f2d33e11ea7d81
DarkDev <darkdev_brand@riseup.net>
2024-07-03 09:57:08 +0300

    refactroing data import, add anchor

--- src/lib/widgets/ui/landing/Features.svelte
+++ src/lib/widgets/ui/landing/Features.svelte
@@ -1,70 +1,89 @@
 <script>
-  const features = [
-    {
-      id: 1,
-      title: 'Lorem Ipsum is simply dummy text of the printing',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
-    },
-    {
-      id: 2,
-      title: 'Lorem Ipsum is simply dummy text of the printing',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
-    },
-    {
-      id: 3,
-      title: 'Lorem Ipsum is simply dummy text of the printing',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
-    },
-    {
-      id: 4,
-      title: 'Lorem Ipsum is simply dummy text of the printing',
-      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
-    },
-  ]
+	import {features} from '$shared';
+
+
+    let step = 0;
+    const prevBoardgame = () => step > 0 ? step-- : step;
+    const nextBoardgame = () => step <= features.length - 2 ? step++ : step;
+
+
+  $: actualFeature = features[step];
 </script>
 
 <section theme="2" class="forces-section">
+      <a name='features' />
+      
       <div class="container-fw-r">
         <div class="col-12">
+          
           <div  class="forces-content-one">
             <div class="km-h2 split-lines">Gamification with mega features</div>
           </div>
+
+
+          
+
           <div class="slider-forces_component">
+            
             <div class="slider-forces_overflow">
               <div>
                 <div class="slider-forces_wrapper">
                   <div class="flex flex-row">
-                      {#each features as feature }
                       <div class="swiper-slide ">
-                        <div class="km-ornamental">0{feature.id}</div>
+                        <div class="km-ornamental">0{actualFeature.id}</div>
                         <div class="line-white"></div>
-                        <div class="km-h4 title-force">{feature.title}</div>
+                        <div class="km-h4 title-force">{actualFeature.title}</div>
                         <div class="padding _1rem"></div>
                         <div class="km-big-p force-text">
-                          {feature.desc}
+                          {actualFeature.desc}
                         </div>
                         <div class="padding _3rem"></div>
                       </div>
-                      {/each}
+
+
+                      <div class="swiper-slide ">
+                        <div class="km-ornamental">0{features[step + 1].id}</div>
+                        <div class="line-white"></div>
+                        <div class="km-h4 title-force">{features[step + 1].title}</div>
+                        <div class="padding _1rem"></div>
+                        <div class="km-big-p force-text">
+                          {features[step + 1].desc}
+                        </div>
+                        <div class="padding _3rem"></div>
+                      </div>
+                      <div class="swiper-slide ">
+                        <div class="km-ornamental">0{actualFeature.id}</div>
+                        <div class="line-white"></div>
+                        <div class="km-h4 title-force">{actualFeature.title}</div>
+                        <div class="padding _1rem"></div>
+                        <div class="km-big-p force-text">
+                          {actualFeature.desc}
+                        </div>
+                        <div class="padding _3rem"></div>
+                      </div>
                   </div>
                 </div>
               </div>
             </div>
             <div element-theme="2" class="slider-gallery_button-wrapper">
-              <a href="#" class="slider-gallery_button swiper-prev w-inline-block">
+              {#if step > 0}
+								<button on:click={()=> prevBoardgame()}  class="slider-gallery_button swiper-prev w-inline-block">
                 <div class="slider-gallery_button-icon w-embed">
                   <svg width="31" height="25" viewBox="0 0 31 25" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1.5 12.5L0.792894 13.2071L0.0857874 12.5L0.792894 11.7929L1.5 12.5ZM29.625 11.5C30.1773 11.5 30.625 11.9477 30.625 12.5C30.625 13.0523 30.1773 13.5 29.625 13.5L29.625 11.5ZM12.0429 24.4571L0.792894 13.2071L2.20711 11.7929L13.4571 23.0429L12.0429 24.4571ZM0.792894 11.7929L12.0429 0.542893L13.4571 1.95711L2.20711 13.2071L0.792894 11.7929ZM1.5 11.5L29.625 11.5L29.625 13.5L1.5 13.5L1.5 11.5Z"></path>
                   </svg>
                 </div>
-              </a>
-              <a href="#" class="slider-gallery_button swiper-next w-inline-block">
+              </button>
+              {/if}
+                {#if step < features.length - 2}
+								<button  on:click={()=> nextBoardgame()}  class="slider-gallery_button swiper-next w-inline-block">
                 <div class="slider-gallery_button-icon is-reversed w-embed">
                   <svg width="31" height="25" viewBox="0 0 31 25" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1.5 12.5L0.792894 13.2071L0.0857874 12.5L0.792894 11.7929L1.5 12.5ZM29.625 11.5C30.1773 11.5 30.625 11.9477 30.625 12.5C30.625 13.0523 30.1773 13.5 29.625 13.5L29.625 11.5ZM12.0429 24.4571L0.792894 13.2071L2.20711 11.7929L13.4571 23.0429L12.0429 24.4571ZM0.792894 11.7929L12.0429 0.542893L13.4571 1.95711L2.20711 13.2071L0.792894 11.7929ZM1.5 11.5L29.625 11.5L29.625 13.5L1.5 13.5L1.5 11.5Z"></path>
                   </svg>
                 </div>
-              </a>
+              </button>
+              {/if}
             </div>
           </div>
           <div  class="forces-wrapper-mobile">
