ec422d765148442a3fcba01e0829227fc1c7de72
DarkDev <darkdev_brand@riseup.net>
2024-07-04 00:22:09 +0300

    intial app route

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	// import {  } from '$lib/shared';
+	// import { } from '$lib/widgets';
+  // import {  } from '$lib/entities'
+</script>
+
+
+<svelte:head>
+  <title>APP - Paleo Stories</title>
+</svelte:head>
+
+
+<style>
+</style>
