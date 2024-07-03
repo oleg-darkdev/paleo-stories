e2e2a666b15307442eb3a5b3c1ac5ba62a3c8f0b
DarkDev <darkdev_brand@riseup.net>
2024-07-04 01:59:32 +0300

    clear legacy code

--- src/lib/widgets/ui/landing/Feedback.svelte
+++ src/lib/widgets/ui/landing/Feedback.svelte
@@ -1,55 +1,74 @@
-<section  theme="1" class="testimonials">
-      <div class="container testimonials">
-        <div class="div-tmn-intro">
-          <div  class="div-block-99"></div>
-          <div  class="testimonials-header-wrapper">
-            <div class="km-tmn-h4">other historical boardgames</div>
-            <div class="padding _1rem"></div>
-            <div class="km-tmn-h2">
-              <span class="span-gray">Still not sure that we can get you results?</span> Ask them instead:
-            </div>
-            <div class="padding _3rem"></div>
-          </div>
-        </div>
-        <div class="slider-main_component">
-          <div  class="div-block-101"></div>
-          <div  class="arrows-wrapper">
-            <a href="#" class="slider-main_button swiper-prev w-inline-block">
-              <div class="slider-gallery_button-icon w-embed">
-                <svg width="31" height="25" viewBox="0 0 31 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1.5 12.5L0.792894 13.2071L0.0857874 12.5L0.792894 11.7929L1.5 12.5ZM29.625 11.5C30.1773 11.5 30.625 11.9477 30.625 12.5C30.625 13.0523 30.1773 13.5 29.625 13.5L29.625 11.5ZM12.0429 24.4571L0.792894 13.2071L2.20711 11.7929L13.4571 23.0429L12.0429 24.4571ZM0.792894 11.7929L12.0429 0.542893L13.4571 1.95711L2.20711 13.2071L0.792894 11.7929ZM1.5 11.5L29.625 11.5L29.625 13.5L1.5 13.5L1.5 11.5Z" fill="currentColor"></path>
-                </svg>
-              </div>
-            </a>
-            <a href="#" class="slider-main_button swiper-next w-inline-block">
-              <div class="slider-gallery_button-icon w-embed">
-                <svg width="31" height="25" viewBox="0 0 31 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M29.5 12.5L30.2071 11.7929L30.9142 12.5L30.2071 13.2071L29.5 12.5ZM1.375 13.5C0.822716 13.5 0.375 13.0523 0.375 12.5C0.375 11.9477 0.822716 11.5 1.375 11.5V13.5ZM18.9571 0.542893L30.2071 11.7929L28.7929 13.2071L17.5429 1.95711L18.9571 0.542893ZM30.2071 13.2071L18.9571 24.4571L17.5429 23.0429L28.7929 11.7929L30.2071 13.2071ZM29.5 13.5H1.375V11.5H29.5V13.5Z" fill="currentColor"></path>
-                </svg>
-              </div>
-            </a>
-          </div>
+<section theme="1" class="testimonials">
+	<div class="testimonials container">
+		<div class="div-tmn-intro">
+			<div class="div-block-99" />
+			<div class="testimonials-header-wrapper">
+				<div class="km-tmn-h4">other historical boardgames</div>
+				<div class="padding _1rem" />
+				<div class="km-tmn-h2">
+					<span class="span-gray">Still not sure that we can get you results?</span> Ask them instead:
+				</div>
+				<div class="padding _3rem" />
+			</div>
+		</div>
+		<div class="slider-main_component">
+			<div class="div-block-101" />
+			<div class="arrows-wrapper">
+				<a href="#" class="slider-main_button swiper-prev w-inline-block">
+					<div class="slider-gallery_button-icon w-embed">
+						<svg
+							width="31"
+							height="25"
+							viewBox="0 0 31 25"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M1.5 12.5L0.792894 13.2071L0.0857874 12.5L0.792894 11.7929L1.5 12.5ZM29.625 11.5C30.1773 11.5 30.625 11.9477 30.625 12.5C30.625 13.0523 30.1773 13.5 29.625 13.5L29.625 11.5ZM12.0429 24.4571L0.792894 13.2071L2.20711 11.7929L13.4571 23.0429L12.0429 24.4571ZM0.792894 11.7929L12.0429 0.542893L13.4571 1.95711L2.20711 13.2071L0.792894 11.7929ZM1.5 11.5L29.625 11.5L29.625 13.5L1.5 13.5L1.5 11.5Z"
+								fill="currentColor"
+							/>
+						</svg>
+					</div>
+				</a>
+				<a href="#" class="slider-main_button swiper-next w-inline-block">
+					<div class="slider-gallery_button-icon w-embed">
+						<svg
+							width="31"
+							height="25"
+							viewBox="0 0 31 25"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M29.5 12.5L30.2071 11.7929L30.9142 12.5L30.2071 13.2071L29.5 12.5ZM1.375 13.5C0.822716 13.5 0.375 13.0523 0.375 12.5C0.375 11.9477 0.822716 11.5 1.375 11.5V13.5ZM18.9571 0.542893L30.2071 11.7929L28.7929 13.2071L17.5429 1.95711L18.9571 0.542893ZM30.2071 13.2071L18.9571 24.4571L17.5429 23.0429L28.7929 11.7929L30.2071 13.2071ZM29.5 13.5H1.375V11.5H29.5V13.5Z"
+								fill="currentColor"
+							/>
+						</svg>
+					</div>
+				</a>
+			</div>
 
-          <div  class="div-block-100"></div>
-          <div  class="">
-            <div class="">
-              <div role="list" class="w-full">
-                <div role="listitem" class="grid grid-cols-3 w-full">
-                  <div class="testimonial-names-div">
-                    <div class="km-tmn-h3">Adrianna Moore</div>
-                    <div class="km-tmn-h4">Senior Program Manager</div>
-                  </div>
-                  <div  class="">
-                    <div class="km-tmn-h3">01</div>
-                  </div>
-                  <div   class="">
-                    <div class="km-tmn-h3">Partnering with KOMMUNA has been a transformative experience. Together, we are making waves in Latino health awareness. Their work in branding, web, and campaigns is sparking crucial conversations in our community. Grateful for their impact and dedication!</div>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+			<div class="div-block-100" />
+			<div class="">
+				<div class="">
+					<div role="list" class="w-full">
+						<div role="listitem" class="grid w-full grid-cols-3">
+							<div class="testimonial-names-div">
+								<div class="km-tmn-h3">Lorem impsum</div>
+								<div class="km-tmn-h4">Lorem impsum</div>
+							</div>
+							<div class="">
+								<div class="km-tmn-h3">01</div>
+							</div>
+							<div class="">
+								<div class="km-tmn-h3">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- src/lib/widgets/ui/landing/TargetAudience.svelte
+++ src/lib/widgets/ui/landing/TargetAudience.svelte
@@ -1,56 +1,58 @@
 <script>
-  const targetAudience = [
-    [
-      {
-        title: 'Lorem Ipsum is simply dummy text',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
-      },
-      {
-        title: 'Lorem Ipsum is simply dummy text',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
-      },
-    ],
-    [
-      {
-        title: 'Lorem Ipsum is simply dummy text',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
-      },
-      {
-        title: 'Lorem Ipsum is simply dummy text',
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
-      },
-    ],
-  ]
+	const targetAudience = [
+		[
+			{
+				title: 'Lorem Ipsum is simply dummy text',
+				desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
+			},
+			{
+				title: 'Lorem Ipsum is simply dummy text',
+				desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
+			}
+		],
+		[
+			{
+				title: 'Lorem Ipsum is simply dummy text',
+				desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
+			},
+			{
+				title: 'Lorem Ipsum is simply dummy text',
+				desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown`
+			}
+		]
+	];
 </script>
 
-<section  theme="2" class="why-kommuna">
-      <div class="center-div">
-        <div class="km-h4 whyus">Gamification of paleontology by using technologies.</div>
-         <a name='use-cases' />
-         
-        <div class="padding _1rem"></div>
-        <div class="km-h3">Say goodbye to the traditional way of doing things. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
-        
-        
-       
+<section theme="2" class="why-paleo-stories">
+	<div class="center-div">
+		<div class="km-h4 whyus">Gamification of paleontology by using technologies.</div>
+		<a name="use-cases" />
 
-        <div class="padding"></div>
-        {#each targetAudience as audience}
-        <div class="row-why">
-          <div class="why-item">
-            <div class="km-h4">{audience[0].title}</div>
-            <div class="line-white why"></div>
-            <div class="km-p">{audience[0].desc}</div>
-          </div>
-          <div  class="why-item">
-            <div class="km-h4 title-experience">{audience[1].title}</div>
-            <div class="line-white why"></div>
-            <div class="km-p">
-            {audience[1].desc}
-            </div>
-          </div>
-        </div>
-        <div class="padding why"></div>
-        {/each}
-      </div>
-    </section>
+		<div class="padding _1rem" />
+		<div class="km-h3">
+			Say goodbye to the traditional way of doing things. Lorem Ipsum is simply dummy text of the
+			printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+			ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+			type specimen book.
+		</div>
+
+		<div class="padding" />
+		{#each targetAudience as audience}
+			<div class="row-why">
+				<div class="why-item">
+					<div class="km-h4">{audience[0].title}</div>
+					<div class="line-white why" />
+					<div class="km-p">{audience[0].desc}</div>
+				</div>
+				<div class="why-item">
+					<div class="km-h4 title-experience">{audience[1].title}</div>
+					<div class="line-white why" />
+					<div class="km-p">
+						{audience[1].desc}
+					</div>
+				</div>
+			</div>
+			<div class="padding why" />
+		{/each}
+	</div>
+</section>
